2028048d8f80290ff5d17d934832d5f21ed40177
hung-313 <graynguyenhai@gmail.com>
2025-09-26 00:39:41 +0700

    Update FormSinhVien.vb

--- FormSinhVien.vb
+++ FormSinhVien.vb
@@ -1,3 +1,5 @@
+
+
 Imports System.Data.SqlClient
 
 Public Class DatabaseConnection
@@ -8,7 +10,8 @@ Public Class DatabaseConnection
     End Function
 End Class
 
-' Thêm sinh viên
+
+
 Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Click
     Dim conn As SqlConnection = DatabaseConnection.GetConnection()
     Dim cmd As New SqlCommand("INSERT INTO SINHVIEN (MaSV, HoTen, NgaySinh, GioiTinh, Email, DienThoai, DiaChi, MaLop) " &
@@ -26,4 +29,38 @@ Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Clic
     cmd.ExecuteNonQuery()
     conn.Close()
     MessageBox.Show("Thêm sinh viên thành công!")
+End Sub
+
+
+
+Private Sub btnSua_Click(sender As Object, e As EventArgs) Handles btnSua.Click
+    Dim conn As SqlConnection = DatabaseConnection.GetConnection()
+    Dim cmd As New SqlCommand("UPDATE SINHVIEN SET HoTen=@HoTen, NgaySinh=@NgaySinh, GioiTinh=@GioiTinh, " &
+                              "Email=@Email, DienThoai=@DienThoai, DiaChi=@DiaChi, MaLop=@MaLop WHERE MaSV=@MaSV", conn)
+
+    cmd.Parameters.AddWithValue("@MaSV", txtMaSV.Text)
+    cmd.Parameters.AddWithValue("@HoTen", txtHoTen.Text)
+    cmd.Parameters.AddWithValue("@NgaySinh", dtpNgaySinh.Value)
+    cmd.Parameters.AddWithValue("@GioiTinh", cboGioiTinh.Text)
+    cmd.Parameters.AddWithValue("@Email", txtEmail.Text)
+    cmd.Parameters.AddWithValue("@DienThoai", txtDienThoai.Text)
+    cmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text)
+    cmd.Parameters.AddWithValue("@MaLop", cboLop.SelectedValue)
+
+    cmd.ExecuteNonQuery()
+    conn.Close()
+    MessageBox.Show("Cập nhật sinh viên thành công!")
+End Sub
+
+
+
+Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
+    Dim conn As SqlConnection = DatabaseConnection.GetConnection()
+    Dim cmd As New SqlCommand("DELETE FROM SINHVIEN WHERE MaSV=@MaSV", conn)
+
+    cmd.Parameters.AddWithValue("@MaSV", txtMaSV.Text)
+
+    cmd.ExecuteNonQuery()
+    conn.Close()
+    MessageBox.Show("Xóa sinh viên thành công!")
 End Sub
